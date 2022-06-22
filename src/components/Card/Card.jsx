6c11c797890b863311061b042e8c871e7ec2decc
Aleksandr <aleksandr_kraynov@hotmail.com>
2022-06-22 18:05:35 +0400

    add card, navbar,fix bug footer and header

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Card.scss';
+import image from '@image/image.png'
+
+
+function Card() {
+    return (
+        <aside className='card'> 
+            <div className='card__image-wrapper'>
+                <img className='card__image' src={image} alt="Изображение напитка"/>  
+            </div>                                                  
+            <div className='card__description'>
+                <h3 className='card__title'>
+                    Blackberry
+                </h3>
+                <h4 className='card__subtitle'>
+                    Освежающий напиток
+                </h4>
+            </div>  
+            <div className='card__degree'>
+                <p className='card__degree-value'>30%</p>
+                <p className='card__degree-title'>Алкоголь</p>
+            </div>          
+        </aside>
+    );
+}
+
+export default Card;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer() {
+    return (
+        <footer class="footer">
+            <div class="container">
+                <button class="button">Поиск</button>
+            </div>         
+        </footer>
+    );
+}
+
+export default Footer;
